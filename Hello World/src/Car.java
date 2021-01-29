87fff5cc97fc5e001598f00dc29ec79042af60d3
tshah2311 <trishla.shah@kinandcarta.com>
2021-01-29 11:48:35 -0600

    First class !

---
+++ Hello World/src/Car.java
@@ -0,0 +1,18 @@
+
+public class Car {
+	String color;
+	String type;
+	
+	public static void start() {
+		
+		System.out.println("get started!");
+		
+	}
+	
+	public void printDescription() {
+		
+		System.out.println("This is a " + color + " " + type);
+		
+	}
+
+}
